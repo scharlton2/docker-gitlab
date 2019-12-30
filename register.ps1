5d994252fb0eef2e603c7951d226d52444a2c591
Scott Charlton <charlton@usgs.gov>
2019-12-29 21:16:32 -0700

    Docker gitlab with runner

---
+++ register.ps1
@@ -0,0 +1,11 @@
+docker exec -it gitlab-runner `
+  gitlab-runner register `
+ --non-interactive `
+ --registration-token ${registration-token} `
+ --locked=false `
+ --description instance-scope `
+ --url http://gitlab-web `
+ --executor docker `
+ --docker-image docker:stable `
+ --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" `
+ --docker-network-mode gitlab-network
